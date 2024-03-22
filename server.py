55af47f0dc507d6d23e09a3150e4639eb05f2efb
esquincaae <edreyae20@gmail.com>
2024-03-21 23:32:11 -0600

    ahora se usa haarcascade de opencv en lugar de la libreria face_recognition

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO, emit
 import numpy as np
 import cv2
 import base64
-import face_recognition
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -13,14 +12,30 @@ def index():
     return "El servidor de detección de rostros está corriendo."
 
 def detect_faces_in_frame(img):
-    # Detecta los rostros en la imagen
-    face_locations = face_recognition.face_locations(img)
-    
-    # Dibuja los recuadros alrededor de los rostros detectados
-    for top, right, bottom, left in face_locations:
-        cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
+    # Aplicar efecto espejo
+    img = cv2.flip(img, 1)
 
-    return img, face_locations
+    # Cargar varios clasificadores Haar
+    cascades = ['haarcascade_frontalface_default.xml', 
+                'haarcascade_frontalface_alt.xml', 
+                'haarcascade_frontalface_alt2.xml', 
+                'haarcascade_profileface.xml']
+    
+    faces_detected = []
+
+    for cascade in cascades:
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + cascade)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for face in faces:
+            if face not in faces_detected:
+                faces_detected.append(face)
+
+    # Dibuja los recuadros alrededor de los rostros detectados en la imagen espejada
+    for (x, y, w, h) in faces_detected:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    return img, faces_detected
 
 @socketio.on('connect')
 def handle_connect():
@@ -37,23 +52,21 @@ def handle_frame(data):
         frame = data['image']
         if not frame.startswith('data:image/jpeg;base64,'):
             raise ValueError("Formato de imagen no esperado")
-        
+
         # Elimina el prefijo y decodifica la imagen
         frame = frame.split(",")[1]
         frame = base64.b64decode(frame)
         nparr = np.frombuffer(frame, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-        img = cv2.flip(img, 1)
-
-        processed_img, face_locations = detect_faces_in_frame(img)
+        processed_img, faces = detect_faces_in_frame(img)
 
         # Codifica la imagen procesada para enviarla de vuelta
         _, buffer = cv2.imencode('.jpg', processed_img)
         encoded_image = base64.b64encode(buffer).decode('utf-8')
 
         print("Procesamiento completado. Enviando respuesta...")
-        emit('response', {'image': encoded_image, 'face_locations': str(face_locations)})
+        emit('response', {'image': encoded_image, 'faces': str(faces)})
     except Exception as e:
         print(f"Error procesando el cuadro: {str(e)}")
         emit('error', {'error': str(e)})
